48f710236f696f36e57a3ab26aa2c9ae9df07021
Ellen Skipper <ellen.skipper@softwire.com>
2018-07-25 15:36:43 +0100

    Knights can take pieces

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -22,7 +22,32 @@ export default class Knight extends Piece {
         moves.push(Square.at(currentLocation.row-1,currentLocation.col+2));
         moves.push(Square.at(currentLocation.row-1,currentLocation.col-2));
 
-        const onBoardMoves = filterOffboardMoves(moves);
+        moves.forEach(square => {
+            if (square.col>-1 && square.col<8 && square.row>-1 && square.row<8) {
+            const blockingPiece = board.getPiece(square);
+            if (blockingPiece) {
+                const index = moves.indexOf(square);
+                moves.splice(index,1);
+                piecesInPath.push(blockingPiece);
+            }
+            }
+        });
+
+        console.log(piecesInPath);
+        piecesInPath.forEach(piece => {
+            if (piece.player === activePlayer || piece.constructor.name === 'King') {
+                const index = piecesInPath.indexOf(piece);
+            piecesInPath.splice(index,1);
+            }
+        });
+        console.log(piecesInPath);
+        
+        let takeableSquares = [];
+        piecesInPath.forEach(piece => {
+            takeableSquares.push(board.findPiece(piece));
+        });
+
+        const onBoardMoves = takeableSquares.concat(filterOffboardMoves(moves));
         return onBoardMoves;
     }
 }
