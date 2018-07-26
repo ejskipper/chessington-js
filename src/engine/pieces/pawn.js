4fc51a4e7adfcff418b077d7ef7345d4198039e4
Ellen Skipper <ellen.skipper@softwire.com>
2018-07-26 11:56:10 +0100

    Pawns can take pieces

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -18,17 +18,38 @@ export default class Pawn extends Piece {
             case Player.WHITE:
                 moves.push(Square.at(currentLocation.row+1,currentLocation.col));
             
-            if (currentLocation.row === 1) {
-                moves.push(Square.at(currentLocation.row+2,currentLocation.col));
-            }
+                if (currentLocation.row === 1) {
+                    moves.push(Square.at(currentLocation.row+2,currentLocation.col));
+                }
+
+                let diagonals = [Square.at(currentLocation.row+1,currentLocation.col-1),Square.at(currentLocation.row+1,currentLocation.col+1)];
+                diagonals.forEach(square => {     
+                if (square.col>-1 && square.col<8 && square.row>-1 && square.row<8) {
+                    const blockingPiece = board.getPiece(square);
+                    if (blockingPiece) {
+                        piecesInPath.push(blockingPiece);
+                    }
+                }
+                });
+
             break;
 
             case Player.BLACK:
                 moves.push(Square.at(currentLocation.row-1,currentLocation.col));
         
-            if (currentLocation.row === 6) {
-                moves.push(Square.at(currentLocation.row-2,currentLocation.col));
-            }
+                if (currentLocation.row === 6) {
+                    moves.push(Square.at(currentLocation.row-2,currentLocation.col));
+                }
+
+                let blackDiag = [Square.at(currentLocation.row-1,currentLocation.col-1),Square.at(currentLocation.row+1,currentLocation.col+1)];
+                blackDiag.forEach(square => {     
+                if (square.col>-1 && square.col<8 && square.row>-1 && square.row<8) {
+                    const blockingPiece = board.getPiece(square);
+                    if (blockingPiece) {
+                        piecesInPath.push(blockingPiece);
+                    }
+                }
+                });
             break;
         }
 
@@ -44,7 +65,19 @@ export default class Pawn extends Piece {
                 }
             }
         });
-        return onBoardMoves;
+        
+        
+        
+
+        functions.removeKingFriendly(piecesInPath,activePlayer);
+        
+        let takeableSquares = [];
+        piecesInPath.forEach(piece => {
+            takeableSquares.push(board.findPiece(piece));
+        });
+
+        const allMoves = takeableSquares.concat(onBoardMoves);
+        return allMoves;
     }
 }
     
